77cc58967835bba86e3e779ce54ca9c3f8af0842
Nutrisari Semangka <130019404+NutrisariSemangka@users.noreply.github.com>
2025-02-13 06:51:17 +0000

    model

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 # torch.classes.__path__ = []
 
 # classs detection
-# import model_detection
-# detektsi = model_detection.Object_Detection()
+import model_detection
+detektsi = model_detection.Object_Detection()
 
 # function
 # load model
